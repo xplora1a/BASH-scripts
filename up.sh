4512ad486c1f3c5c2392e0558d648ab3a3c9b8de
Stuart Ward <stuart.ward@bcs.org>
2026-06-04 23:19:16 +0100

    Changes to be committed: 	modified:   up.sh 	new file:   zoom-version.sh
    Added  testing for zoom updates.
    If the version installed is not the latest,
    it will download the latest version to the Downloads folder.

--- up.sh
+++ up.sh
@@ -11,6 +11,16 @@ if [ -x /usr/bin/snap ]; then
 	echo "Snap updates"
 	sudo snap refresh
 fi
+echo "Check the version of Zoom available"
+location=$(curl -L -i -s --max-redirs 0 https://zoom.us/client/latest/zoom_amd64.deb | grep "location:")
+version=$(echo "$location" | sed -E 's#.*prod/([0-9.]+)/.*#\1#')
+#get the version installed
+installed=$(dpkg -s zoom | grep "Version:" | awk '{print $2}')
+echo "Installed version: $installed Latest version: $version"
+if [ "$installed" != "$version" ]; then
+    echo "Zoom is not up to date. Getting latest version."
+    curl -L -o "${HOME}"/Downloads/zoom_amd64-"${version}".deb https://zoom.us/client/latest/zoom_amd64.deb    
+fi
 if [ -x ~/Documents/settings/"${sname}"/ssh ]; then
 	echo "Backing up ssh config"
 	cp -auv ~/.ssh/* ~/Documents/settings/"${sname}"/ssh/
---
+++ zoom-version.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# Get the version of Zoom available as that latest download
+location=$(curl -L -i -s --max-redirs 0 https://zoom.us/client/latest/zoom_amd64.deb | grep "location:")
+version=$(echo "$location" | sed -E 's#.*prod/([0-9.]+)/.*#\1#')
+#get the version installed
+installed=$(dpkg -s zoom | grep "Version:" | awk '{print $2}')
+echo "Installed version: $installed"
+echo "Latest version: $version"
+if [ "$installed" != "$version" ]; then
+    echo "Zoom is not up to date. Getting latest version."
+    curl -L -o "${HOME}"/Downloads/zoom_amd64-"${version}".deb https://zoom.us/client/latest/zoom_amd64.deb    
+fi
