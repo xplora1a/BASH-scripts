72837cf446ffb5b0d451cf269217f4f69a8fd7eb
Stuart Ward <stuart.ward@bcs.org>
2026-09-24 14:54:37 +0100

    Added instal option to zoom-version.sh
    Changed files:
    M	zoom-version.sh

--- zoom-version.sh
+++ zoom-version.sh
@@ -1,7 +1,21 @@
 #! /bin/bash
+install=false
+case "$1" in
+    -i|--install)
+        install=true
+        ;;
+    "")
+        ;;
+    *)
+        echo "Usage: $0 [--install|-i]"
+        exit 2
+        ;;
+esac
+
 # Get the version of Zoom available as that latest download
 location=$(curl -L -i -s --max-redirs 0 https://zoom.us/client/latest/zoom_amd64.deb | grep "location:")
 version=$(echo "$location" | sed -E 's#.*prod/([0-9.]+)/.*#\1#')
+package="${HOME}/Downloads/zoom_amd64-${version}.deb"
 #get the version installed
 installed=$(dpkg -s zoom | grep "Version:" | awk '{print $2}')
 if [ -z "$installed" ]; then
@@ -11,10 +25,15 @@ fi
 echo "Installed version: $installed"
 echo "Latest version: $version"
 if [ "$installed" != "$version" ]; then
-    if [ -f "${HOME}"/Downloads/zoom_amd64-"${version}".deb ]; then
+    if [ -f "$package" ]; then
         echo "Zoom is not up to date. Latest version already downloaded."
     else
         echo "Zoom is not up to date. Getting latest version."
-        curl -L -o "${HOME}"/Downloads/zoom_amd64-"${version}".deb https://zoom.us/client/latest/zoom_amd64.deb
+        curl -L -o "$package" https://zoom.us/client/latest/zoom_amd64.deb
     fi
+fi
+
+if $install && [ "$installed" != "$version" ]; then
+    echo "Installing Zoom ${version}."
+    sudo apt install -y "$package"
 fi
