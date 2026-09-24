9a7ccda3f117d74c3c36d5d4b366648de016557c
Stuart Ward <stuart.ward@bcs.org>
2026-09-24 14:35:05 +0100

    Download the latest version even if not installed.
    Changed files:
    M	zoom-version.sh

--- zoom-version.sh
+++ zoom-version.sh
@@ -7,15 +7,14 @@ installed=$(dpkg -s zoom | grep "Version:" | awk '{print $2}')
 if [ -z "$installed" ]; then
     echo "Zoom is not installed."
     installed="none"
-else
-    echo "Installed version: $installed"
-    echo "Latest version: $version"
-    if [ "$installed" != "$version" ]; then
-        if [ -f "${HOME}"/Downloads/zoom_amd64-"${version}".deb ]; then
-            echo "Zoom is not up to date. Latest version already downloaded."
-        else
-            echo "Zoom is not up to date. Getting latest version."
-            curl -L -o "${HOME}"/Downloads/zoom_amd64-"${version}".deb https://zoom.us/client/latest/zoom_amd64.deb
-        fi
+fi
+echo "Installed version: $installed"
+echo "Latest version: $version"
+if [ "$installed" != "$version" ]; then
+    if [ -f "${HOME}"/Downloads/zoom_amd64-"${version}".deb ]; then
+        echo "Zoom is not up to date. Latest version already downloaded."
+    else
+        echo "Zoom is not up to date. Getting latest version."
+        curl -L -o "${HOME}"/Downloads/zoom_amd64-"${version}".deb https://zoom.us/client/latest/zoom_amd64.deb
     fi
 fi
